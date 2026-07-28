c5f7e5eeec0c3f757f1a192d4683d8484e33838d
secureanytime <jameswoodinfo@gmail.com>
2026-07-28 19:16:39 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -8,6 +8,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'alabaster' # Screenshot wala classic white theme
 
-html_context = { 'metatags': <meta name="google-site-verification" content="Yel_C69xoq0MiKn3qV4oh4aahOYq5GevwyXt6ETpHaU" />' }
+
 
 
