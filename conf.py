d6884660f28e4ef7e315ccff5c55cecc1a581e55
secureanytime <jameswoodinfo@gmail.com>
2026-07-28 19:24:04 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -8,6 +8,17 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'alabaster' # Screenshot wala classic white theme
 
+# conf.py
 
+html_title = "Download Kaspersky Total Security in your Windows PC"
+html_short_title = "Download Kaspersky Total Security"
+
+# Meta Tags Configuration
+html_context = {
+    'metatags': '''
+        <meta name="description" content="To download purchased Kaspersky, sign in to your My Kaspersky account, go to Subscriptions, and click Download.">
+        <meta name="Download Kaspersky Total Security" content="docs, guide, setup, tutorial">
+    '''
+}
 
 
